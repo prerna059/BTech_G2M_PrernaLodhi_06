f7531b14c5ff739ea78f09f5b677a5df5b2f52e1
prerna059 <prernalodhi2006@gmail.com>
2026-04-03 12:22:20 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,14 @@
+
+
+ #include<stdio.h>
+/*this program is written to find the perimeter of rectangle 
+n finally this is the first one that didnt give any errors TT in C*/
+int main () {
+    int a,b;
+    printf("The length of the rectangle is ");
+    scanf("%d", &a);
+    printf("The breadth of the rectangle is ");
+    scanf("%d", &b);
+    printf("The perimeter of the rectangle is %d", a+b);
+    return 0;
+}
