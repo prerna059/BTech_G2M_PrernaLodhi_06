a84a26270ecdcfad55b4cbfee19906937cbc73bd
prerna059 <prernalodhi2006@gmail.com>
2026-04-04 21:35:01 +0530

    Add files via upload

---
+++ temperature.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<math.h>
+int main() 
+{
+  float T;
+  printf("Enter the temperature in centigrade :");
+  scanf("%f",&T);
+  printf("the temperature in farenheit is %f",T/5*9+32);
+  return 0;
+}
+  
