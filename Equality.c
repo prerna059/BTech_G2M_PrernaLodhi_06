c89b7a05636549e7473e99b16eaecc6f9cea9065
prerna059 <prernalodhi2006@gmail.com>
2026-04-05 21:00:27 +0530

    Add files via upload

---
+++ Equality.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+{
+  int a;
+  int b;
+  printf("Enter the first number : \n");
+  scanf("%d",&a);
+  printf("Enter the second number : \n");
+  scanf("%d",&b);
+  if(a==b)
+  {
+    printf("The numbers are equal");
+  }
+  else{
+    printf("They are not equal");
+  }
+
+
+  return 0;
+}
+  
